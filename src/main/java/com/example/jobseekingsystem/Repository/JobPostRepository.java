fcb6168006ac3df748b484fff84110f78fb8ecf4
salem ALmotiry <60990492+salemALmotiry@users.noreply.github.com>
2024-12-02 14:42:56 +0300

    update

--- src/main/java/com/example/jobseekingsystem/Repository/JobPostRepository.java
+++ src/main/java/com/example/jobseekingsystem/Repository/JobPostRepository.java
@@ -2,6 +2,8 @@ package com.example.jobseekingsystem.Repository;
 
 import com.example.jobseekingsystem.Model.JobPost;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface JobPostRepository extends JpaRepository<JobPost, Long> {
 }
